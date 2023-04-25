46a1946c8ea3b9a2abb093ad99c1ab6cdb37770e
carloshg24 <carloscdb16edu@gmail.com>
2023-04-25 15:44:58 +0200

    gfgu
    igih

--- category.php
+++ category.php
@@ -98,7 +98,7 @@ if(isset($_POST['add_to_cart'])){
 
 <section class="products">
 
-   <h1 class="title">Elige a tu gusto</h1>
+   <h1 class="title">Elige a tu gust</h1>
 
    <div class="box-container">
 
